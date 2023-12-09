ff84a3f7ec3e56b604773578f4346ddef325de28
Alexander <atlexeide@gmail.com>
2023-12-09 20:40:11 +0100

    Update game logic

--- js/script.js
+++ js/script.js
@@ -1,70 +1,93 @@
-let game = (function () {
-    let gameboard = [
-        ['X', '', ''],
-        ['X', '', ''],
-        ['X', '', '']
-    ];
 
-    checkForWinCondition = function (marker) {
+const game = {
+    player: {
+        points: null,
+        marker: '',
+    },
+    gameboard: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ],
+    displayConsoleBoard: function () {
+        console.log(this.gameboard[0]);
+        console.log(this.gameboard[1]);
+        console.log(this.gameboard[2]);
+    },
+    //For later
+    cahcedDOM: null
+    ,
+    init: function (marker) {
+        this.chooseMarker(marker)
+
+    },
+    chooseMarker: function (choice) {
+        let chosenMarker = choice.toUpperCase();
+        console.log(chosenMarker)
+        chosenMarker === 'X' ? this.player.marker = 'X' :
+            chosenMarker === 'O' ? this.player.marker = 'O' :
+                console.log('Please choose X or O');
+        if (chosenMarker === 'X' || chosenMarker === 'O') {
+            console.log(`You chose ${this.player.marker}!`)
+        }
+        this.playTurn();
+    },
+    playTurn: function () {
+        if (this.player.marker === 'O') {
+            this.getCpuPlay();
+            console.log(this.checkForWinCondition('X'));
+        }
+        else {
+            this.getPlayerPlay();
+        }
+        displayConsoleBoard()
+    },
+    getCpuPlay: function () {
+        let cpuMarker = '';
+        if (this.player.marker === 'X') { cpuMarker = 'O' }
+        if (this.player.marker === 'O') { cpuMarker = 'X' }
+        let row = Math.floor(Math.random() * 3)
+        let tile = Math.floor(Math.random() * 3)
+        if (this.gameboard[row][tile] === '') {
+            this.gameboard[row].splice(tile, 1, cpuMarker)
+            console.clear()
+            console.log(`CPU placed ${cpuMarker} at ${row}, ${tile}`)
+            this.displayConsoleBoard()
+            this.checkForWinCondition(cpuMarker)
+
+        }
+        else { this.getCpuPlay() }
+        this.getPlayerPlay()
+    },
+    getPlayerPlay: function () {
+        let row = prompt('Choose row number')
+        let tile = prompt('Choose tile number')
+        if (this.gameboard[row][tile] === '') {
+            this.gameboard[row].splice(tile, 1, this.player.marker);
+            console.clear()
+            console.log(`PLAYER placed ${this.player.marker} at ${row}, ${tile}`)
+            this.displayConsoleBoard()
+            this.checkForWinCondition(this.player.marker)
+        }
+        else { this.getPlayerPlay() }
+        this.getCpuPlay()
+    },
+    checkForWinCondition: function (marker) {
         let currentMarker = marker;
         if (//Check for horizontal win
-            gameboard[0][0] === currentMarker && gameboard[0][1] === currentMarker && gameboard[0][2] === currentMarker ||
-            gameboard[1][0] === currentMarker && gameboard[1][1] === currentMarker && gameboard[1][2] === currentMarker ||
-            gameboard[2][0] === currentMarker && gameboard[2][1] === currentMarker && gameboard[2][2] === currentMarker ||
+            this.gameboard[0][0] === currentMarker && this.gameboard[0][1] === currentMarker && this.gameboard[0][2] === currentMarker ||
+            this.gameboard[1][0] === currentMarker && this.gameboard[1][1] === currentMarker && this.gameboard[1][2] === currentMarker ||
+            this.gameboard[2][0] === currentMarker && this.gameboard[2][1] === currentMarker && this.gameboard[2][2] === currentMarker ||
             // Check for vertical win
-            gameboard[0][0] === currentMarker && gameboard[1][0] === currentMarker && gameboard[2][0] === currentMarker ||
-            gameboard[0][1] === currentMarker && gameboard[1][1] === currentMarker && gameboard[2][1] === currentMarker ||
-            gameboard[0][2] === currentMarker && gameboard[1][2] === currentMarker && gameboard[2][2] === currentMarker ||
+            this.gameboard[0][0] === currentMarker && this.gameboard[1][0] === currentMarker && this.gameboard[2][0] === currentMarker ||
+            this.gameboard[0][1] === currentMarker && this.gameboard[1][1] === currentMarker && this.gameboard[2][1] === currentMarker ||
+            this.gameboard[0][2] === currentMarker && this.gameboard[1][2] === currentMarker && this.gameboard[2][2] === currentMarker ||
             // Check for diagonal win
-            gameboard[0][0] === currentMarker && gameboard[1][1] === currentMarker && gameboard[2][2] === currentMarker ||
-            gameboard[0][2] === currentMarker && gameboard[1][1] === currentMarker && gameboard[2][0] === currentMarker) { return `${currentMarker} win` }
-    };
-
-    let currentRound = 0;
-    getCurrentRound = function () {
-        return currentRound;
+            this.gameboard[0][0] === currentMarker && this.gameboard[1][1] === currentMarker && this.gameboard[2][2] === currentMarker ||
+            this.gameboard[0][2] === currentMarker && this.gameboard[1][1] === currentMarker && this.gameboard[2][0] === currentMarker) {
+            alert(`${currentMarker} win`)
+        }
+        else { return `Undecided, keep playing` }
     }
-    playTurn = function (player, tile) {
-        currentRound += 1;
-
-        return;
-    };
-    return {
-        gameboard: gameboard,
-        currentRound: getCurrentRound,
-        checkForWinCondition: checkForWinCondition,
-    };
-})();
-
-const markerOptions = ['X', 'O'];
-const x = markerOptions[0];
-const o = markerOptions[1];
-
-
-
-
-function createPlayer(chosenMarker) {
-    const getChosenMarker = function () {
-        marker = chosenMarker;
-        return marker;
-    };
-    let points = 0;
-    const addRoundWin = function () {
-        points += 1;
-        return points;
-    };
-    return {
-        points: points,
-        marker: getChosenMarker,
-        addRoundWin: addRoundWin
-    };
-};
-const player = createPlayer(x);
-const cpu = createPlayer(o);
-
-
-console.log(player.marker());
-console.log(game.gameboard[0]);
-console.log(game.gameboard[1]);
-console.log(game.gameboard[2]);
-console.log(game.checkForWinCondition(cpu.marker()));
+}
+game.init(prompt())
